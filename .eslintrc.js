74ad09c336e0b50b9ca53da6834065a6985f7093
Sadot Cortes <sadotcortes@gmail.com>
2021-10-12 21:28:31 -0500

    [++] Config files
    - Eslint
      - rc
      - ignore
    - Prettier
      - rc
      - ignore
    - Gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    common: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "always"],
+  },
+};
