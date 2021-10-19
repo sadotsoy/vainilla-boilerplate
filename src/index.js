4e8db51cf8f5ea38f10deb0a88372c539b44e79a
Sadot Cortes <sadotcortes@gmail.com>
2021-10-19 13:56:37 -0500

    [++] canvas element and mainDraw call

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-(()=> {
-  console.log('init');
+import './styles/globals.css';
+import mainDraw from './draws/';
+
+(() => {
+  const canvas = document.createElement('canvas');
+  canvas.id = 'canvas';
+  document.body.appendChild(canvas);
+
+  // DRAW
+  mainDraw();
 })();
