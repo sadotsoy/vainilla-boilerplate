874a7f17f4b0fa152e4a1cfd2c662a35cbf5f583
Sadot Cortes <sadotcortes@gmail.com>
2021-10-19 16:20:29 -0500

    [~~] best approach to resize the canvas

--- src/utils/canvas.js
+++ src/utils/canvas.js
@@ -1,14 +1,14 @@
 export function resizeCanvasToDisplaySize(canvas) {
-  // Lookup the size the browser is displaying the canvas in CSS pixels
-  const dWidth = canvas.clientWidth;
-  const dHeight = canvas.clientHeight;
 
-  // Check if the canvas is not the same size.
+  const { width, height } = canvas.getBoundingClientRect();
+
+  const dpr = window.devicePixelRatio;
+  const dWidth = Math.round(width * dpr);
+  const dHeight = Math.round(height * dpr);
+
   const needResize = canvas.width !== dWidth || canvas.height !== dHeight;
 
   if (needResize) {
-    // Make the canvas the same size
-    console.log('sad: rezize');
     canvas.width = dWidth;
     canvas.height = dHeight;
   }
